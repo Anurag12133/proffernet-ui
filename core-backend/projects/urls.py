3702adcf707e6a01c0333bc8ec3f5a83f2400224
Anurag12133 <anurajput62616@gmail.com>
2024-12-09 23:46:33 +0530

    chore: media access added

--- core-backend/projects/urls.py
+++ core-backend/projects/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProjectCreateAPIView
+from .views import ProjectCreateAPIView, ProjectListAPIView
 
 urlpatterns = [
     path('create/', ProjectCreateAPIView.as_view(), name='project-create'),
+    path('list/', ProjectListAPIView.as_view(), name='project-list'),
 ]
--- core-backend/projects/views.py
+++ core-backend/projects/views.py
@@ -2,12 +2,20 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Project
-from .serializers import ProjectSerializer
+from .serializers import ProjectSerializer, ProjectCreateSerializer
+
 
 class ProjectCreateAPIView(APIView):
     def post(self, request, *args, **kwargs):
-        serializer = ProjectSerializer(data=request.data)
+        serializer = ProjectCreateSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProjectListAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        projects = Project.objects.prefetch_related('files').all()
+        serializer = ProjectSerializer(projects, many=True)
+        return Response(serializer.data)
