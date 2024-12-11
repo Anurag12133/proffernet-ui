254a65f30e7c0d055a657d89421aa45a0351ac6f
Anurag12133 <anurajput62616@gmail.com>
2024-12-12 00:33:19 +0530

    refactor: models.py and serilizer.py in projects

--- core-backend/projects/models.py
+++ core-backend/projects/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 
 
+
+
 class Project(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
-    tech_stack = models.JSONField(default=list)
+    tech_stack = models.JSONField(default=list, blank=True)
 
     def __str__(self):
         return self.title
 
-class ProjectImage(models.Model):
-    project = models.ForeignKey(Project, related_name='images', on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='project_images/')
+class ProjectFile(models.Model):
+    project = models.ForeignKey(Project, related_name='files', on_delete=models.CASCADE, blank=True)
+    file = models.ImageField(upload_to='project_files/')
 
     def __str__(self):
         return f"Image for {self.project.title}"
