3cc42b0ed14ea201dc82e4847a3c9f2e0f29e48b
Anurag12133 <anurajput62616@gmail.com>
2024-12-19 23:23:04 +0530

    add: project_list url

--- core-backend/projects/urls.py
+++ core-backend/projects/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import ProjectCreateAPIView
+from .views import ProjectCreateAPIView, ProjectListAPIView
 
 urlpatterns = [
     path('create/', ProjectCreateAPIView.as_view(), name='project-create'),
-    
+    path('projectlist/', ProjectListAPIView.as_view(), name='project-list')
 ]
