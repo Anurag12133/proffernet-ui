c173c6d0561a07b692b0b7ff62722bead920df78
Anurag12133 <anurajput62616@gmail.com>
2024-12-27 03:46:24 +0530

    remove some unused code

--- core-frontend/next.config.ts
+++ core-frontend/next.config.ts
@@ -11,13 +11,6 @@ module.exports = {
       "127.0.0.1",
     ],
   },
-
-  env: {
-    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-    GITHUB_SECRET_KEY: process.env.GITHUB_SECRET_KEY,
-    JWT_SECRET: process.env.JWT_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-  },
 };
 
 const nextConfig: NextConfig = {
