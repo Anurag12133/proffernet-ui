56dc84093f59230854a1be7eb06c673f977b7039
Anurag12133 <anurajput62616@gmail.com>
2025-01-12 19:25:18 +0530

    add : project list api added

--- core-backend/projects/urls.py
+++ core-backend/projects/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProjectListCreateView
+from .views import ProjectListCreateView, ProjeDetailsView
 
 urlpatterns = [
     path('create/', ProjectListCreateView.as_view(), name='project-create'),
+    path('list/', ProjeDetailsView.as_view(), name='project-list'),
 ]
--- core-backend/projects/views.py
+++ core-backend/projects/views.py
@@ -1,7 +1,10 @@
 from rest_framework import generics, permissions
 from rest_framework.parsers import MultiPartParser, FormParser
 from .models import Project
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
 from .serializers import ProjectSerializer
+from rest_framework.response import Response
 
 class ProjectListCreateView(generics.ListCreateAPIView):
     """
@@ -15,3 +18,10 @@ class ProjectListCreateView(generics.ListCreateAPIView):
     def get_queryset(self):
         # Restrict projects to the logged-in user
         return Project.objects.filter(user=self.request.user)
+
+class ProjeDetailsView(APIView):
+    permission_classes = [AllowAny]
+    def get(self, request, *args, **kwargs):
+        projects = Project.objects.all()
+        serializer = ProjectSerializer(projects, many=True)
+        return Response(serializer.data, status=200)
