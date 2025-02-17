4ea0aa28b60678a94100200de50b0d72104567e4
Anurag12133 <anurajput62616@gmail.com>
2025-02-17 15:35:49 +0530

    remove: unused files in pervious frontend folder

--- frontend/app/components/common/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Navbar } from "./Navbar";
-export { default as List } from "./List";
-export { default as NavLink } from "./NavLink";
-export { default as Spinner } from "./Spinner";
--- frontend/lib/index.ts
+++
@@ -1,6 +0,0 @@
-import continueWithSocialAuth from "./social-auth";
-
-export const continueWithGoogle = () =>
-  continueWithSocialAuth("google-oauth2", "google");
-export const continueWithGithub = () =>
-  continueWithSocialAuth("github", "github");
